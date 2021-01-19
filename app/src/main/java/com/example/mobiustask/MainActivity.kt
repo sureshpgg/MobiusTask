55b0eecde23450e52fc7e6d1dbb397580c5890e4
sureshpgg <sureshpgg1@gmail.com>
2021-01-20 00:35:32 +0530

    data capture

--- app/src/main/java/com/example/mobiustask/MainActivity.kt
+++ app/src/main/java/com/example/mobiustask/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.mobiustask
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.mobiustask.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,11 +14,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+    private fun setupPermissions() {
+        val permission = ContextCompat.checkSelfPermission(this,
+            Manifest.permission.INTERNET)
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i("PERMISSION", "Permission to record denied")
+            makeRequest()
+        }
+    }
+    private val INTERNET_REQUEST_CODE = 101
+    private fun makeRequest() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.INTERNET),
+            INTERNET_REQUEST_CODE)
     }
 
 }
